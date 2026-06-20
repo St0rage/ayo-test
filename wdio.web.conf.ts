9021f99f89288b77e3840e08994932fac2dee4bd
Dani Yudistira Maulana <St0rage>
2026-06-20 17:57:31 +0700

    update config

--- wdio.web.conf.ts
+++ wdio.web.conf.ts
@@ -50,19 +50,26 @@ export const config: WebdriverIO.Config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
+
+  protocol: process.env.HUB_PROTO as string | "http",
+  hostname: process.env.HUB_HOSTNAME as string | "localhost",
+  port: Number(process.env.HUB_PORT) | 4444,
+  path: process.env.HUB_PATH as string | "/wd/hub",
+
   capabilities: [
     {
       browserName: "chrome",
       "goog:chromeOptions": {
         args: [
           "--start-maximized",
-          "--headless=new",
-          "--no-sandbox",
-          "--disable-dev-shm-usage",
-          "--disable-gpu",
-          "--window-size=1920,1080",
+          // "--headless=new",
+          // "--no-sandbox",
+          // "--disable-dev-shm-usage",
+          // "--disable-gpu",
+          // "--window-size=1920,1080",
         ],
       },
+      "wdio:enforceWebDriverClassic": true,
     },
   ],
 
